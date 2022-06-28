5d6244eecc3c7f2100ea6495a0b6f124a6e9c1c8
innihans <innihans@gmail.com>
2022-06-28 18:38:50 +0900

    ㅁㅁ

--- src/main/java/com/jindule/jinduleapi/Controller.java
+++ src/main/java/com/jindule/jinduleapi/Controller.java
@@ -3,6 +3,7 @@ package com.jindule.jinduleapi;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +16,10 @@ public class Controller {
     public ResponseEntity<?> login(@RequestBody String message) {
         log.error("API REQUESTED: {}", message);
         return new ResponseEntity<> ("", HttpStatus.OK);
+    }
+
+    @GetMapping("/ap_adm/Ap_Admin_Ping")
+    public ResponseEntity<?> ping() {
+        return ResponseEntity.ok().build();
     }
 }
